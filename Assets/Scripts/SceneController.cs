246e41e4259fa9eb7a4e3395f1722f384251fe75
J Beckett <jennybeckett2@hotmail.com>
2022-02-14 20:29:34 -0800

    done lab 5

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -9,22 +9,35 @@ public class SceneController : MonoBehaviour
     private GameObject enemyPrefab;
     private GameObject enemy;
     private Vector3 spawnPoint = new Vector3(0, 0, 5);
-
-
+    private int numEnemies;
+    private GameObject[] enemyArr;
     // Start is called before the first frame update
     void Start()
     {
-        
+        numEnemies = 14;
+
+        enemyArr = new GameObject[numEnemies];
+        for (int i = 0; i < numEnemies; i++)
+        {
+            enemyArr[i] = null;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (enemy == null) { 
-            enemy = Instantiate(enemyPrefab) as GameObject; 
-            enemy.transform.position = spawnPoint;
-            float angle = Random.Range(0, 360); 
-            enemy.transform.Rotate(0, angle, 0);
+
+        for (int i = 0; i < numEnemies; i++)
+        {
+            if (enemyArr[i] == null)
+            {
+                enemy = Instantiate(enemyPrefab) as GameObject;
+                enemy.transform.position = spawnPoint;
+                float angle = Random.Range(0, 360);
+                enemy.transform.Rotate(0, angle, 0);
+
+                enemyArr[i] = enemy;
+            }
         }
     }
 }
